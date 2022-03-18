8b61ec29fb5c7e2a01e756a97b2911c78d31f15b
Mers77 <emerson00lima00@gmail.com>
2022-03-17 21:16:17 -0300

    finished

---
+++ function.js
@@ -0,0 +1,53 @@
+let nameSpaceship = prompt('Qual nome deseja dar a sua nave?')
+
+let spaceshipVelocity = 0
+
+let chosenOption
+
+function showMenu() {
+  let option
+  while (option != '1' && option != '2' && option != '3' && option != '4') {
+    option = prompt(
+      'O que deseja fazer?\n' +
+        '1 - Acelerar a nave em 5km/s\n' +
+        '2 - Desacelerar a nave em 5km/s\n' +
+        '3 - Imprimir dados de bordo\n' +
+        '4 - Sair da nave'
+    )
+  }
+  return option
+}
+
+function speedUp(velocity) {
+  let newVelocity = velocity + 5
+  return newVelocity
+}
+
+function slowDown(velocity) {
+  let newVelocity = velocity - 5
+  if (newVelocity < 0) {
+    newVelocity = 0
+  }
+  return newVelocity
+}
+
+function printSpaceshipData(name, velocity) {
+  alert('Nome da nave: ' + name + '\nVelocidade: ' + velocity + 'km/s')
+}
+
+do {
+  chosenOption = showMenu()
+  switch (chosenOption) {
+    case '1':
+      spaceshipVelocity = speedUp(spaceshipVelocity)
+      break
+    case '2':
+      spaceshipVelocity = slowDown(spaceshipVelocity)
+      break
+    case '3':
+      printSpaceshipData(nameSpaceship, spaceshipVelocity)
+      break
+    default:
+      alert('Encerrando o dashboard')
+  }
+} while (chosenOption != '4')
